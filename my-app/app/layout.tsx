20a0f06d1154bc1bebbc6bf977d7ff3a9e33cd16
pdluuu <pl.podcast.08@gmail.com>
2024-05-03 15:44:35 +0700

    before merge

--- my-app/app/layout.tsx
+++ my-app/app/layout.tsx
@@ -2,8 +2,9 @@ import './globals.css';
 import { Inter as FontSans } from 'next/font/google';
 import { ThemeProvider } from '@/components/theme-provider';
 import { cn } from '@/lib/utils';
-import React from 'react';
+import React, { useState } from 'react';
 import { GoogleOAuthProvider } from '@react-oauth/google';
+import { ModeToggle } from '@/components/theme-button';
 
 const fontSans = FontSans({
     subsets: ['latin'],
@@ -15,8 +16,9 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
         <html lang="en" suppressHydrationWarning>
             <head />
             <body className={cn('min-h-screen bg-background font-sans antialiased', fontSans.variable)}>
-                <ThemeProvider attribute="class" defaultTheme="dark" enableSystem disableTransitionOnChange>
+                <ThemeProvider attribute="class" defaultTheme="light" enableSystem disableTransitionOnChange>
                     <GoogleOAuthProvider clientId={process.env.NEXT_PUBLIC_CLIENT_ID as string}>
+                        <ModeToggle></ModeToggle>
                         {children}
                     </GoogleOAuthProvider>
                 </ThemeProvider>
