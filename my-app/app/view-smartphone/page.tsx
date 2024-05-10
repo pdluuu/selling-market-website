2a00f374209113ecd2b5c7a788de9561387e1cd4
AndyKhanhs <linhlaai84@gmail.com>
2024-05-10 19:57:58 +0700

    add filter for all categoies

--- my-app/app/view-smartphone/page.tsx
+++ my-app/app/view-smartphone/page.tsx
@@ -1,24 +1,29 @@
+'use client'
 import Footer from '../footer/page';
 import { MenubarDemo } from '../dashboard/category/page';
 import DisplayedItem from '../dashboard/displayeditem/page';
 import Header from '../dashboard/header/page';
 import BreadcrumbWithCustomSeparator from '../dashboard/breadcrumb/page';
 import Filter from '../dashboard/filter/page';
+import { useState } from 'react';
 
 export default function Dashboard() {
-    
-
+    const [price, setPrice] = useState<number>(-1);
+    const [brands, setBrands] = useState<string[]>(['Oppo', 'Samsung', 'Huawei', 'Xiaomi', 'Apple', 'Nokia']);
+    const filterPrice = (newPrice: number, brand?: string) => {
+        setPrice(newPrice);
+        if (brand!==undefined) setBrands([brand]);
+        else setBrands(['Oppo', 'Samsung', 'Huawei', 'Xiaomi', 'Apple', 'Nokia']);
+    };
     return (
         <div className="flex flex-col items-center">
             <Header />
             <MenubarDemo />
-            <BreadcrumbWithCustomSeparator category="Điện thoại"/>
-            <Filter category="Điện thoại"/>
-            <DisplayedItem brand="Oppo" category="Điện thoại"  />
-            <DisplayedItem brand="Samsung" category="Điện thoại" />
-            <DisplayedItem brand="Apple" category="Điện thoại" />
-            <DisplayedItem brand="Nokia" category="Điện thoại" />
-            <DisplayedItem brand="Xiaomi" category="Điện thoại" />
+            <BreadcrumbWithCustomSeparator category="Điện thoại" />
+            <Filter category="Điện thoại" filterPrices={filterPrice} />
+            {brands.map((brand, index) => {
+                return <DisplayedItem key={index} category="Điện thoại" brand={brand} price={price} />;
+            })}
             <Footer />
         </div>
     );
--- my-app/app/view-watch/page.tsx
+++ my-app/app/view-watch/page.tsx
@@ -1,22 +1,29 @@
+'use client'
 import Footer from '../footer/page';
 import { MenubarDemo } from '../dashboard/category/page';
 import DisplayedItem from '../dashboard/displayeditem/page';
 import Header from '../dashboard/header/page';
 import BreadcrumbWithCustomSeparator from '../dashboard/breadcrumb/page';
 import Filter from '../dashboard/filter/page';
+import { useState } from 'react';
 
 export default function Dashboard() {
-    
-
+    const [price, setPrice] = useState<number>(-1);
+    const [brands, setBrands] = useState<string[]>(['Garmin', 'Samsung', 'Apple']);
+    const filterPrice = (newPrice: number, brand?: string) => {
+        setPrice(newPrice);
+        if (brand!==undefined) setBrands([brand]);
+        else setBrands(['Garmin', 'Samsung', 'Apple']);
+    };
     return (
         <div className="flex flex-col items-center">
             <Header />
             <MenubarDemo />
-            <BreadcrumbWithCustomSeparator category="Đồng hồ"/>
-            <Filter category="Đồng hồ"/>
-            <DisplayedItem brand="Garmin" category="Đồng hồ" />
-            <DisplayedItem brand="Samsung" category="Đồng hồ" />
-            <DisplayedItem brand="Apple" category="Đồng hồ" />
+            <BreadcrumbWithCustomSeparator category="Đồng hồ" />
+            <Filter category="Đồng hồ" filterPrices={filterPrice} />
+            {brands.map((brand, index) => {
+                return <DisplayedItem key={index} category="Đồng hồ" brand={brand} price={price} />;
+            })}
             <Footer />
         </div>
     );
